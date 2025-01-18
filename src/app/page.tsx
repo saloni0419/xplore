27764f02717293193d4a78c9c72f065c07471f2c
saloni0419 <salonisingh0419@gmail.com>
2025-01-19 01:51:13 +0530

    dark mode light mode done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedOut , SignedIn , SignInButton , UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="m-4">
       {/*these are the compenents of ClerkProvider */}
 
       <SignedOut> {/*if user is signed out show them signin*/}
         <SignInButton mode="modal">
-          <button className="bg-red-500">
-              Sign In
-          </button>
+          <Button>Sign in</Button>
           </SignInButton>
       </SignedOut>
 
       <SignedIn> {/*if user is signed in show them userbutton*/}
         <UserButton />
       </SignedIn>
+
+      <ModeToggle />
+      <Button variant={"secondary"}>Click Me</Button>
     </div>
   );
 }
---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,12 @@
+"use client" 
+{/* to make it client component (not server component)*/}
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
