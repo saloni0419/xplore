bbbab1f09dfdd4b8213ba68afb99ae119213de09
saloni0419 <salonisingh0419@gmail.com>
2025-01-19 01:10:20 +0530

    authentication done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { SignedOut , SignedIn , SignInButton , UserButton} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>hello</h1>
+
+      {/*these are the compenents of ClerkProvider */}
+
+      <SignedOut> {/*if user is signed out show them signin*/}
+        <SignInButton mode="modal">
+          <button className="bg-red-500">
+              Sign In
+          </button>
+          </SignInButton>
+      </SignedOut>
+
+      <SignedIn> {/*if user is signed in show them userbutton*/}
+        <UserButton />
+      </SignedIn>
     </div>
-  )
+  );
 }
