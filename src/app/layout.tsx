c0f49abdfecd5bb90013f2a2473fc14f8ba1d843
saloni0419 <salonisingh0419@gmail.com>
2025-01-19 05:30:59 +0530

    navbar done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,20 +28,31 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-        {children}
-        </ThemeProvider>
-      </body>
-    </html>
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+                {/* container to center the component */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedOut , SignedIn , SignInButton , UserButton} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="m-4">
-      {/*these are the compenents of ClerkProvider */}
-
-      <SignedOut> {/*if user is signed out show them signin*/}
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-          </SignInButton>
-      </SignedOut>
-
-      <SignedIn> {/*if user is signed in show them userbutton*/}
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
-      <Button variant={"secondary"}>Click Me</Button>
+      <h1>home page content</h1>
     </div>
   );
 }
